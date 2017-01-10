bdeb3ad43388a76a71a95b25d1d74dc841b580ce
Juan Sebastian Reyes <juansefreak@gmail.com>
2017-01-09 19:05:46 -0500

    se mdifica la sintaxis del documento del index del javaScript del lado del cliente

--- src/home/template.js
+++ src/home/template.js
@@ -3,7 +3,7 @@ var layout = require('../layout');
 var picture = require('../picture-card');
 
 module.exports= function(pictures){
-  var element = yo ` <div class= "template-home">
+  var element = yo `<div class= "template-home">
     ${pictures.map(function(pic){
       return picture;
     })}
